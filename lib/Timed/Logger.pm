2bafd2ec0b0ec64e2b8d54d488ffe3982af0b0e4
Nikolay Martynov <nmartynov@shutterstock.com>
2013-09-18 17:25:05 -0400

    Version number increase

--- lib/Timed/Logger.pm
+++ lib/Timed/Logger.pm
@@ -16,11 +16,11 @@ Timed::Logger - store events for later analysis.
 
 =head1 VERSION
 
-Version 0.0.1
+Version 0.0.2
 
 =cut
 
-our $VERSION = '0.0.1';
+our $VERSION = '0.0.2';
 
 =head1 SYNOPSIS
 
