9868360c6d834830e162e2786b9fe5475fc73bdb
Nikolay Martynov <nmartynov@shutterstock.com>
2013-09-18 17:28:53 -0400

    use module as readme

--- lib/Timed/Logger.pm
+++ lib/Timed/Logger.pm
@@ -16,11 +16,11 @@ Timed::Logger - store events for later analysis.
 
 =head1 VERSION
 
-Version 0.0.2
+Version 0.0.3
 
 =cut
 
-our $VERSION = '0.0.2';
+our $VERSION = '0.0.3';
 
 =head1 SYNOPSIS
 
